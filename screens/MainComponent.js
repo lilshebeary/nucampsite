262015d2acdee621a6330f874cedd8fc34e205a8
lilshebeary <cirijaye@gmail.com>
2024-01-31 04:53:08 -0800

    add stack navigation to app and Main

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,13 +1,48 @@
-import { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import { StyleSheet, View, Platform } from 'react-native';
+import Constants from 'expo-constants';
 import DirectoryScreen from './DirectoryScreen';
+import CampsiteInfoScreen from './CampsiteInfoScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
+const DirectoryNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName='Directory'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#5637DD'
+        },
+        headerTintColor: '#fff'
+      }}
+    >
+        <Stack.Screen
+         name='Directory'
+         component={DirectoryScreen}
+         options={{ title: 'Campsite Directory' }}
+      />
+      <Stack.Screen
+        name='CampsiteInfo'
+        component={CampsiteInfoScreen}
+        options={({ route }) => ({
+          title: route.params.campsite.name
+        })}
+      />
+    </Stack.Navigator>
+  )
+}
 
 const Main = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES)
   return (
-      <DirectoryScreen campsites={campsites} />
+    <View style={{ 
+      flex: 1,
+      paddingTop: 
+        Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+     }}
+     >
+      <DirectoryNavigator />
+    </View>
+      
   )
 }
 
