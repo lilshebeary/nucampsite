df63734daa27fd02d210a41dc16e13a5284c9da9
lilshebeary <cirijaye@gmail.com>
2024-01-31 05:47:50 -0800

    add home navigator

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -3,18 +3,33 @@ import Constants from 'expo-constants';
 import DirectoryScreen from './DirectoryScreen';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './HomeScreen';
+
+const screenOptions = {
+  headerStyle: { backgroundColor: '#5637DD' },
+  headerTintColor: '#fff'
+}
+
+const HomeNavigator = () => {
+  const Stack = createStackNavigator();
+  return(
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen 
+        name='Home'
+        component={HomeScreen}
+        options={{ title: 'Home' }}
+      />
+    </Stack.Navigator>
+  )
+}
 
 const DirectoryNavigator = () => {
   const Stack = createStackNavigator();
   return (
     <Stack.Navigator
       initialRouteName='Directory'
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#5637DD'
-        },
-        headerTintColor: '#fff'
-      }}
+      screenOptions={screenOptions}
     >
         <Stack.Screen
          name='Directory'
