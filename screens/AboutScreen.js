6596cc6f8cfa50c050d5cc211d364de5b58b4810
lilshebeary <cirijaye@gmail.com>
2024-02-02 04:21:06 -0800

    add Contact and about page with navigators

---
+++ screens/AboutScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+
+const AboutScreen = () => {
+  return (
+    <ScrollView>
+      <Text>AboutScreen</Text>
+    </ScrollView>
+  )
+}
+
+export default AboutScreen
+
+const styles = StyleSheet.create({})
---
+++ screens/ContactScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+
+const ContactScreen = () => {
+  return (
+    <ScrollView>
+      <Text>ContactScreen</Text>
+    </ScrollView>
+  )
+}
+
+export default ContactScreen
+
+const styles = StyleSheet.create({})
