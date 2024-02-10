829c21b2b41affec12c5f9f4aef8dfac78c63ebc
lilshebeary <cirijaye@gmail.com>
2024-02-10 08:40:33 -0800

    add modal and rating for comments use rating to render comments

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -9,16 +9,11 @@ const RenderCampsite = (props) => {
             <Card containerStyle={styles.cardContainer}>
                 <Card.Image source={{ uri: baseUrl + campsite.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
-                        <Text
-                            style={{
-                                color: 'white',
-                                textAlign: 'center',
-                                fontSize: 20
-                            }}
-                       >{campsite.name}</Text>
+                        <Text style={styles.cardText}>{campsite.name}</Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
+               <View style={styles.cardRow}>
                 <Icon 
                     name={props.isFavorite ? 'heart' : 'heart-o'}
                     type='font-awesome'
@@ -31,6 +26,15 @@ const RenderCampsite = (props) => {
                          : props.markFavorite()
                     }
                 />
+                <Icon 
+                    name='pencil'
+                    type='font-awesome'
+                    color='#5637dd'
+                    raised
+                    reverse
+                    onPress={() => props.onShowModal()}
+                />
+                </View>
             </Card>
         )
     }
@@ -44,5 +48,22 @@ const styles = StyleSheet.create({
         padding: 0,
         margin: 0,
         marginBottom: 20
+    },
+    cardRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    cardText: {
+        textShadowColor: 'rgba(0, 0, 0, 1)',
+        textShadowOffset:{
+            width: -1, height: 1
+        },
+        textShadowRadius: 20,
+        textAlign: 'center',
+        color: 'white',
+        fontSize:20
     }
 })
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,76 +1,164 @@
-import { StyleSheet, FlatList, Text, View } from 'react-native'
-import RenderCampsite from '../features/campsites/RenderCampsite';
-import { useSelector, useDispatch } from 'react-redux'; 
-import { toggleFavorite } from '../features/favorites/favoritesSlice'
+import { useState } from "react";
+import { StyleSheet, FlatList, Text, View, Button, Modal } from "react-native";
+import RenderCampsite from "../features/campsites/RenderCampsite";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleFavorite } from "../features/favorites/favoritesSlice";
+import { Rating, Input } from 'react-native-elements';
 
 const CampsiteInfoScreen = ({ route }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [rating, setRating] = useState(5);
+  const [author, setAuthor] = useState('');
+  const [text, setText] = useState('');
+
   const { campsite } = route.params;
-  const comments = useSelector(state => state.comments);
-  const favorites = useSelector(state => state.favorites);
+  const comments = useSelector((state) => state.comments);
+  const favorites = useSelector((state) => state.favorites);
   const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    const newComment = {
+      author,
+      rating,
+      text,
+      campsiteId: campsite.id
+  };
+  console.log(newComment);
+  setShowModal(!showModal);
+  }
+ 
+  const resetForm = () => {
+    setAuthor('');
+    setRating(5);
+    setText('');
+};
 
   const renderCommentItem = ({ item }) => {
     return (
       <View style={styles.commentItem}>
-        <Text style={styles.commentStyle}>{item.text}</Text>
-        <Text style={styles.ratingStyle}>{item.rating}</Text>
-        <Text style={styles.authorStyle}>
+         <Rating
+          readonly
+          type="star"
+          startingValue={item.rating}
+          imageSize={10}
+          style={{ paddingVertical: '5%', alignItems: 'flex-start' }}
+        />
+         <Text style={styles.commentStyle}>{item.text}</Text> 
+        <Text style={styles.authorStyle}> 
           {` -- ${item.author}, ${item.date} `}
-        </Text>    
+        </Text>
       </View>
-    )
-  }
+    );
+  };
 
   return (
-    <FlatList
-      data={comments.commentsArray.filter(
-        (comment) => comment.campsiteId === campsite.id
-      )}
-      renderItem={renderCommentItem}
-      keyExtractor={item => item.id.toString()}
-      contentContainerStyle={{
-        marginHorizontal: 20,
-        paddingVertical: 20
-      }}
-      ListHeaderComponent={
-        <>
-          <RenderCampsite 
-            campsite={campsite} 
-            isFavorite={favorites.includes(campsite.id)}
-            markFavorite={() => dispatch(toggleFavorite(campsite.id))}
-          /> 
-          <Text style={styles.commentsTitle}>Comments</Text>
-        </>
-      }
-    />
-   
-  )
-}
+    <>
+      <FlatList
+        data={comments.commentsArray.filter(
+          (comment) => comment.campsiteId === campsite.id
+        )}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={{
+          marginHorizontal: 20,
+          paddingVertical: 20,
+        }}
+        ListHeaderComponent={
+          <>
+            <RenderCampsite
+              campsite={campsite}
+              isFavorite={favorites.includes(campsite.id)}
+              markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+              onShowModal={() => setShowModal(!showModal)}
+            />
+            <Text style={styles.commentsTitle}>Comments</Text>
+          </>
+        }
+      />
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={showModal}
+        onRequestClose={() => setShowModal(!showModal)}
+      >
+        <View style={styles.modal}>
+        <Rating
+          type="star"
+          startingValue={rating}
+          imageSize={40}
+          onFinishRating={(rating) => setRating(rating)}
+          style={{ paddingVertical: 10 }}
+        />
+        <Input 
+          placeholder="author"
+          leftIcon={{ type: 'font-awesome', name: 'user-o'}}
+          leftIconContainerStyle
+          onChangeText={(author) => setAuthor(author)}
+          value={author}
+        />
+        <Input 
+          placeholder="text"
+          leftIcon={{ type: 'font-awesome', name: 'comment'}}
+          leftIconContainerStyle
+          onChangeText={(text) => setText(text)}
+          value={text}
+        />
+        <View style={{margin: 10}}>
+        <Button 
+          title='Submit' 
+          color='#5637dd'
+          onPress={() => {
+            handleSubmit();
+            resetForm();
+          }}
+        />
+        </View>
+        
+          <View style={{margin: 10}}>
+            <Button 
+              onPress={() => {
+                setShowModal(!showModal)
+                resetForm()
+              }}
+              color='#808080'
+              title="Cancel"
+            ></Button>
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
+};
 
 export default CampsiteInfoScreen;
 
 const styles = StyleSheet.create({
   commentsTitle: {
-    textAlign: 'center',
-    backgroundColor: '#fff',
+    textAlign: "center",
+    backgroundColor: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#43484d',
+    fontWeight: "bold",
+    color: "#43484d",
     padding: 10,
-    paddingTop: 30
+    paddingTop: 30,
   },
   commentItem: {
     paddingVertical: 10,
-    paddingHorizontal: 20, 
-    backgroundColor: '#fff'
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
   },
   commentStyle: {
-    fontSize: 14
+    fontSize: 14,
   },
   ratingStyle: {
-    fontSize: 12
+    fontSize: 12,
   },
   authorStyle: {
-    fontSize: 12
-  }
-})
+    fontSize: 12,
+  },
+  modal: {
+    justifyContent: 'center',
+    margin: 20
+  },
+
+});
