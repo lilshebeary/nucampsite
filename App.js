4bb2ea5876c714c0f9eb9fedabd4ae5c2dcd1fae
lilshebeary <cirijaye@gmail.com>
2024-02-15 05:12:13 -0800

    add persist for asyncStorage

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import "react-native-gesture-handler";
 import Main from "./screens/MainComponent";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import Loading from "./components/LoadingComponent";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Main />
-      </NavigationContainer>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <NavigationContainer>
+          <Main />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
