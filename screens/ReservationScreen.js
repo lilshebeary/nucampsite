dc87218ff5c16fcda805ffaae8a863b391161a29
lilshebeary <cirijaye@gmail.com>
2024-02-20 18:53:19 -0800

    syntax correction

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -18,7 +19,6 @@ const ReservationScreen = () => {
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
    
-
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
         setShowCalendar();
@@ -44,6 +44,7 @@ const ReservationScreen = () => {
                 text: 'OK',
                 onPress: () => {
                     console.log('Search Availabilty ..'), 
+                    presentLocalNotification(date.toLocaleDateString('en-US'))
                     resetForm();
                 }
               }
@@ -58,6 +59,32 @@ const ReservationScreen = () => {
         setDate(new Date());
         setShowCalendar(false);
     };
+      
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            })
+        };
+        let permissions = await Notifications.getPermissionsAsync();
+         if(permissions.granted){
+            permissions = await Notifications.requestPermissionsAsync()
+         }
+         if(permissions.granted){
+            sendNotification();
+         }
+    }
 
     return (
         <ScrollView>
