62a4f1c5d9fe41f41607f6537f0b72a8911c2a8c
lilshebeary <cirijaye@gmail.com>
2024-02-23 21:54:05 -0800

    add image for register

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView, Image } from "react-native";
 import { CheckBox, Input, Button, Icon } from "react-native-elements";
 import * as SecureStore from "expo-secure-store";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as ImagePicker from 'expo-image-picker';
+import { baseUrl } from '../shared/baseUrl';
+import logo from '../assets/images/logo.png'
 
 const LoginTab = ({ navigation }) => {
   const [username, setUsername] = useState("");
@@ -108,6 +111,7 @@ const RegisterTab = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [remember, setRemember] = useState(false);
+  const [imageUrl, setImageUrl] = useState(baseUrl + 'images/logo.png')
 
   const handleRegister = () => {
     const userInfo = {
@@ -134,9 +138,33 @@ const RegisterTab = () => {
       );
     }
   };
+
+  const getImageFromCamera = async () => {
+    const cameraPermission =
+    await ImagePicker.requestCameraPermissionsAsync();
+    if(cameraPermission.status === 'granted') {
+      const capturedImage = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [1, 1]
+      });
+      if (capturedImage.assets){
+        console.log(capturedImage.assets[0])
+        setImageUrl(capturedImage.assets[0].uri);
+      }
+    }
+  }
+
   return (
     <ScrollView>
       <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image 
+            source={{ uri: imageUrl }}
+            loadingIndicatorSource={logo}
+            style={styles.image}
+          />
+          <Button title='Camera' onPress={getImageFromCamera}/>
+        </View>
         <Input
           placeholder="Username"
           leftIcon={{ type: "font-awesome", name: "user-o" }}
@@ -251,6 +279,21 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     margin: 10,
+  },
+  imageContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'space-evenly',
+    margin: 10
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    borderColor: '#808080',
+    borderWidth: .5,
+  
   },
   formIcon: {
     marginRight: 10,
